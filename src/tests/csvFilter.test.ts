4d5070814f2db7a736f2ce7cb7301677511b37e7
Jose Garrera <josegarrera@gmail.com>
2024-07-18 09:20:36 -0300

    refactor: move header into describe scope

--- src/tests/csvFilter.test.ts
+++ src/tests/csvFilter.test.ts
@@ -1,8 +1,8 @@
 import { CsvFilter } from '../core/csvFilter';
 describe('CSV Filter', () => {
+  const header =
+    'Num_factura, Fecha, Bruto, Neto, IVA, IGIC, Concepto, CIF_cliente, NIF_cliente';
   it('allows correct lines only', () => {
-    const header =
-      'Num_factura, Fecha, Bruto, Neto, IVA, IGIC, Concepto, CIF_cliente, NIF_cliente';
     const invoiceLine =
       '1,02/05/2021,1000,790,21,,ACER Laptop,B76430134,';
     const csvFilter = CsvFilter.create([header, invoiceLine]);
@@ -11,8 +11,6 @@ describe('CSV Filter', () => {
   });
 
   it('removes lines with both IVA and IGIC with values', () => {
-    const header =
-      'Num_factura, Fecha, Bruto, Neto, IVA, IGIC, Concepto, CIF_cliente, NIF_cliente';
     const invoiceLine =
       '1,02/05/2021,1000,790,21,20,ACER Laptop,B76430134,';
     const csvFilter = CsvFilter.create([header, invoiceLine]);
@@ -21,8 +19,6 @@ describe('CSV Filter', () => {
   });
 
   it('removes lines with wrong net value calculation', () => {
-    const header =
-      'Num_factura, Fecha, Bruto, Neto, IVA, IGIC, Concepto, CIF_cliente, NIF_cliente';
     const invoiceLine =
       '1,02/05/2021,1000,780,21,,ACER Laptop,B76430134,';
     const csvFilter = CsvFilter.create([header, invoiceLine]);
@@ -31,8 +27,6 @@ describe('CSV Filter', () => {
   });
 
   it('removes lines with both CIF and NIF with values', () => {
-    const header =
-      'Num_factura, Fecha, Bruto, Neto, IVA, IGIC, Concepto, CIF_cliente, NIF_cliente';
     const invoiceLine =
       '1,02/05/2021,1000,790,21,,ACER Laptop,B76430134,1234';
     const csvFilter = CsvFilter.create([header, invoiceLine]);
@@ -41,8 +35,6 @@ describe('CSV Filter', () => {
   });
 
   it('removes lines with the same invoice number', () => {
-    const header =
-      'Num_factura, Fecha, Bruto, Neto, IVA, IGIC, Concepto, CIF_cliente, NIF_cliente';
     const invoiceLines = [
       '1,02/05/2021,1000,790,21,,ACER Laptop,B76430134,',
       '1,02/05/2021,1000,790,21,,ACER Laptop,,1234',
@@ -67,8 +59,6 @@ describe('CSV Filter', () => {
   });
 
   it('removes lines with none IVA and IGIC with values', () => {
-    const header =
-      'Num_factura, Fecha, Bruto, Neto, IVA, IGIC, Concepto, CIF_cliente, NIF_cliente';
     const invoiceLine =
       '1,02/05/2021,1000,790,,,ACER Laptop,B76430134,';
     const csvFilter = CsvFilter.create([header, invoiceLine]);
