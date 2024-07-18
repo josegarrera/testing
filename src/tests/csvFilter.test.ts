4f269f6d8aa0bf940506e92409c1de25a9a6a28c
Jose Garrera <josegarrera@gmail.com>
2024-07-18 12:03:52 -0300

    refactor: add factory method to tests

--- src/tests/csvFilter.test.ts
+++ src/tests/csvFilter.test.ts
@@ -2,43 +2,37 @@ import { CsvFilter } from '../core/csvFilter';
 describe('CSV Filter', () => {
   const header =
     'Num_factura, Fecha, Bruto, Neto, IVA, IGIC, Concepto, CIF_cliente, NIF_cliente';
+
   it('allows correct lines only', () => {
-    const invoiceLine =
-      '1,02/05/2021,1000,790,21,,ACER Laptop,B76430134,';
+    const invoiceLine = lineFactory({});
     const csvFilter = CsvFilter.create([header, invoiceLine]);
     const result = csvFilter.filteredLines;
     expect(result).toEqual([header, invoiceLine]);
   });
 
   it('removes lines with both IVA and IGIC with values', () => {
-    const invoiceLine =
-      '1,02/05/2021,1000,790,21,20,ACER Laptop,B76430134,';
+    const invoiceLine = lineFactory({ igic: '20' });
     const csvFilter = CsvFilter.create([header, invoiceLine]);
     const result = csvFilter.filteredLines;
     expect(result).toEqual([header]);
   });
 
   it('removes lines with wrong net value calculation', () => {
-    const invoiceLine =
-      '1,02/05/2021,1000,780,21,,ACER Laptop,B76430134,';
+    const invoiceLine = lineFactory({ net: '780' });
     const csvFilter = CsvFilter.create([header, invoiceLine]);
     const result = csvFilter.filteredLines;
     expect(result).toEqual([header]);
   });
 
   it('removes lines with both CIF and NIF with values', () => {
-    const invoiceLine =
-      '1,02/05/2021,1000,790,21,,ACER Laptop,B76430134,1234';
+    const invoiceLine = lineFactory({ nif: 'AB1234' });
     const csvFilter = CsvFilter.create([header, invoiceLine]);
     const result = csvFilter.filteredLines;
     expect(result).toEqual([header]);
   });
 
   it('removes lines with the same invoice number', () => {
-    const invoiceLines = [
-      '1,02/05/2021,1000,790,21,,ACER Laptop,B76430134,',
-      '1,02/05/2021,1000,790,21,,ACER Laptop,,1234',
-    ];
+    const invoiceLines = [lineFactory({}), lineFactory({})];
     const csvFilter = CsvFilter.create([header, ...invoiceLines]);
     const result = csvFilter.filteredLines;
     expect(result).toEqual([header]);
@@ -51,28 +45,52 @@ describe('CSV Filter', () => {
   });
 
   it('should give an error if you give a one line list because it has no headers', () => {
-    const invoiceLine =
-      '1,02/05/2021,1000,790,21,,ACER Laptop,B76430134,';
+    const invoiceLine = lineFactory({});
     expect(() => CsvFilter.create([invoiceLine])).toThrow(
       'No header found'
     );
   });
 
   it('removes lines with none IVA and IGIC with values', () => {
-    const invoiceLine =
-      '1,02/05/2021,1000,790,,,ACER Laptop,B76430134,';
+    const invoiceLine = lineFactory({
+      iva: emptyValue,
+      igic: emptyValue,
+    });
     const csvFilter = CsvFilter.create([header, invoiceLine]);
     const result = csvFilter.filteredLines;
     expect(result).toEqual([header]);
   });
 
   it('removes lines if IVA or IGIC has characters in it', () => {
-    const invoiceLines = [
-      '1,02/05/2021,1000,790,2a1,,ACER Laptop,B76430134,',
-      '2,02/05/2021,1000,790,,2b1,ACER Laptop,B76430134,',
-    ];
-    const csvFilter = CsvFilter.create([header, ...invoiceLines]);
+    const invoiceLine = lineFactory({ iva: '2a1' });
+    const csvFilter = CsvFilter.create([header, invoiceLine]);
     const result = csvFilter.filteredLines;
     expect(result).toEqual([header]);
   });
+
+  const emptyValue = '';
+  const lineFactory = ({
+    net = '790',
+    iva = '21',
+    igic = emptyValue,
+    cif = 'B76430134',
+    nif = emptyValue,
+  }): string => {
+    const number: string = '1';
+    const date: string = '02/05/2021';
+    const gross: string = '1000';
+    const description: string = 'ACER Laptop';
+    const invoiceLine = [
+      number,
+      date,
+      gross,
+      net,
+      iva,
+      igic,
+      description,
+      cif,
+      nif,
+    ].join(',');
+    return invoiceLine;
+  };
 });
