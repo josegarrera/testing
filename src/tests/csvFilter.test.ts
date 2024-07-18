fa62d5fd51767144a27a320dcae17c5f0cf69782
Jose Garrera <josegarrera@gmail.com>
2024-07-18 07:47:13 -0300

    red: repeated invoice number

--- src/tests/csvFilter.test.ts
+++ src/tests/csvFilter.test.ts
@@ -39,4 +39,16 @@ describe('CSV Filter', () => {
     const result = csvFilter.filteredLines;
     expect(result).toEqual([header]);
   });
+
+  it('removes lines with the same invoice number', () => {
+    const header =
+      'Num_factura, Fecha, Bruto, Neto, IVA, IGIC, Concepto, CIF_cliente, NIF_cliente';
+    const invoiceLines = [
+      '1,02/05/2021,1000,790,21,,ACER Laptop,B76430134,',
+      '1,02/05/2021,1000,790,21,,ACER Laptop,,1234',
+    ];
+    const csvFilter = CsvFilter.create([header, ...invoiceLines]);
+    const result = csvFilter.filteredLines;
+    expect(result).toEqual([header]);
+  });
 });
