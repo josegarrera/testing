c8f8d8beba64afad73b0cbf44338098494abd3a0
Jose Garrera <josegarrera@gmail.com>
2024-07-28 09:20:33 -0300

    refactor to generic solution

--- src/core/factorial.ts
+++ src/core/factorial.ts
@@ -1,3 +1,7 @@
 export const factorialOf = (number: number): number => {
-  return 1;
+  let factorial: number = 1;
+  for (let i = number; i > 0; i--) {
+    factorial = factorial * i;
+  }
+  return factorial;
 };
--- src/tests/factorial.test.ts
+++ src/tests/factorial.test.ts
@@ -4,4 +4,19 @@ describe('Factorial of a number', () => {
     const number: number = 0;
     expect(factorialOf(number)).toBe(1);
   });
+
+  it('should return 1 if the number is 1', () => {
+    const number: number = 1;
+    expect(factorialOf(number)).toBe(1);
+  });
+
+  it('should return 120 if the number is 5', () => {
+    const number: number = 5;
+    expect(factorialOf(number)).toBe(120);
+  });
+
+  it('should return 720 if the number is 6', () => {
+    const number: number = 6;
+    expect(factorialOf(number)).toBe(720);
+  });
 });
