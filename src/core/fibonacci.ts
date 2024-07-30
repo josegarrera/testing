1bfc6508bbda93adee365cf485554e1e399560d7
Jose Garrera <josegarrera@gmail.com>
2024-07-30 13:18:39 -0300

    green: refactor to non recursive solution

--- src/core/fibonacci.ts
+++ src/core/fibonacci.ts
@@ -1,3 +1,13 @@
 export const fibonacciOf = (number: number): number => {
-  return 0;
+  if (number === 0) return 0;
+  if (number === 1) return 1;
+  let auxiliary1 = 0;
+  let auxiliary2 = 1;
+  let fibonacciNumber = 0;
+  for (let i = 2; i <= number; i++) {
+    fibonacciNumber = auxiliary1 + auxiliary2;
+    auxiliary1 = auxiliary2;
+    auxiliary2 = fibonacciNumber;
+  }
+  return fibonacciNumber;
 };
--- src/tests/fibonacci.test.ts
+++ src/tests/fibonacci.test.ts
@@ -3,4 +3,12 @@ describe('Fibonacci Number', () => {
   it('should return 0 when the input is 0', () => {
     expect(fibonacciOf(0)).toBe(0);
   });
+
+  it('should return 1 when the input is 1', () => {
+    expect(fibonacciOf(1)).toBe(1);
+  });
+
+  it('should return 2 when the input is 3', () => {
+    expect(fibonacciOf(3)).toBe(2);
+  });
 });
