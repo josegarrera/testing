27e93f3018fd17e86b0f484c7c69989e16c42a51
Jose Garrera <josegarrera@gmail.com>
2024-08-20 18:04:30 -0300

    refactor: recursive solution

--- src/core/primeFactors.ts
+++ src/core/primeFactors.ts
@@ -1,34 +1,12 @@
 export const primeFactorsOf = (number: number): number[] => {
-  const factors: number[] = [];
-  number = getEvenFactors(number, factors);
-  number = getOddFactors(number, factors);
-  handlePrimeNumGreaterTwo(number, factors);
+  let factor = 2;
+  while (number % factor !== 0) {
+    ++factor;
+  }
+  const factors = [factor];
+  const remainder = number / factor;
+  if (remainder > 1) {
+    return factors.concat(primeFactorsOf(remainder));
+  }
   return factors;
-};
-
-const getEvenFactors = (number: number, factors: number[]) => {
-  while (number % 2 === 0) {
-    factors.push(2);
-    number = number / 2;
-  }
-  return number;
-};
-
-const getOddFactors = (number: number, factors: number[]) => {
-  for (let i = 3; i <= Math.sqrt(number); i += 2) {
-    while (number % i === 0) {
-      factors.push(i);
-      number = number / i;
-    }
-  }
-  return number;
-};
-
-const handlePrimeNumGreaterTwo = (
-  number: number,
-  factors: number[]
-) => {
-  if (number > 2) {
-    factors.push(number);
-  }
 };
