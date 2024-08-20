0f2540be7cc26c0715c23760e5381814234db8cb
Jose Garrera <josegarrera@gmail.com>
2024-08-20 18:03:25 -0300

    refactor: extract functions

--- src/core/primeFactors.ts
+++ src/core/primeFactors.ts
@@ -1,26 +1,34 @@
 export const primeFactorsOf = (number: number): number[] => {
   const factors: number[] = [];
+  number = getEvenFactors(number, factors);
+  number = getOddFactors(number, factors);
+  handlePrimeNumGreaterTwo(number, factors);
+  return factors;
+};
 
-  // Check for number of 2s that divide number
+const getEvenFactors = (number: number, factors: number[]) => {
   while (number % 2 === 0) {
     factors.push(2);
     number = number / 2;
   }
+  return number;
+};
 
-  // Number must be odd at this point, so we can skip even numbers
+const getOddFactors = (number: number, factors: number[]) => {
   for (let i = 3; i <= Math.sqrt(number); i += 2) {
-    // While i divides number, add i and divide number
     while (number % i === 0) {
       factors.push(i);
       number = number / i;
     }
   }
+  return number;
+};
 
-  // This condition is to handle the case when number is a prime number
-  // greater than 2
+const handlePrimeNumGreaterTwo = (
+  number: number,
+  factors: number[]
+) => {
   if (number > 2) {
     factors.push(number);
   }
-
-  return factors;
 };
