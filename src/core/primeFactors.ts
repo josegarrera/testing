5372b71ef3cd9f9895f3000f85f4b3d3c931b7a1
Jose Garrera <josegarrera@gmail.com>
2024-08-20 17:13:11 -0300

    green: all test cases passing

--- src/core/primeFactors.ts
+++ src/core/primeFactors.ts
@@ -1,6 +1,26 @@
 export const primeFactorsOf = (number: number): number[] => {
-  if (number === 9) return [3, 3];
-  if (number === 3) return [3];
-  const quantityOfTwos = Math.floor(number / 2);
-  return new Array(quantityOfTwos).fill(2);
+  const factors: number[] = [];
+
+  // Check for number of 2s that divide number
+  while (number % 2 === 0) {
+    factors.push(2);
+    number = number / 2;
+  }
+
+  // Number must be odd at this point, so we can skip even numbers
+  for (let i = 3; i <= Math.sqrt(number); i += 2) {
+    // While i divides number, add i and divide number
+    while (number % i === 0) {
+      factors.push(i);
+      number = number / i;
+    }
+  }
+
+  // This condition is to handle the case when number is a prime number
+  // greater than 2
+  if (number > 2) {
+    factors.push(number);
+  }
+
+  return factors;
 };
--- src/tests/primeFactors.test.ts
+++ src/tests/primeFactors.test.ts
@@ -6,13 +6,22 @@ describe('Prime Factors', () => {
   it('when 4 is passed it should return [2,2]', () => {
     expect(primeFactorsOf(4)).toMatchObject([2, 2]);
   });
-  it('when 6 is passed it should return [2,2,2]', () => {
-    expect(primeFactorsOf(6)).toMatchObject([2, 2, 2]);
+  it('when 8 is passed it should return [2,2,2]', () => {
+    expect(primeFactorsOf(8)).toMatchObject([2, 2, 2]);
   });
   it('when 3 is passed it should return [3]', () => {
     expect(primeFactorsOf(3)).toMatchObject([3]);
   });
   it('when 9 is passed it should return [3,3]', () => {
     expect(primeFactorsOf(9)).toMatchObject([3, 3]);
+  });
+  it('when 6 is passed it should return [2,3]', () => {
+    expect(primeFactorsOf(6)).toMatchObject([2, 3]);
+  });
+  it('when 25 is passed it should return [5,5]', () => {
+    expect(primeFactorsOf(25)).toMatchObject([5, 5]);
+  });
+  it('when 1155 is passed it should return [3,5,7,11]', () => {
+    expect(primeFactorsOf(1155)).toMatchObject([3, 5, 7, 11]);
   });
 });
