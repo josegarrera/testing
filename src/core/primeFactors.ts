9ec13d0d769b81f50ce13eed5e44f6b74a829e5a
Jose Garrera <josegarrera@gmail.com>
2024-08-20 18:20:05 -0300

    refactor: extract method

--- src/core/primeFactors.ts
+++ src/core/primeFactors.ts
@@ -1,11 +1,15 @@
 export const primeFactorsOf = (number: number): number[] => {
-  if (number < 1)
-    throw new Error('Only positive numbers are allowed');
+  checkForPositiveNumber(number);
   const prime = findSmallestPrime(number);
   const remainder = number / prime;
   return remainder <= 1
     ? [prime]
     : [prime].concat(primeFactorsOf(remainder));
+};
+
+const checkForPositiveNumber = (number: number) => {
+  if (number < 1)
+    throw new Error('Only positive numbers are allowed');
 };
 
 const findSmallestPrime = (number: number) => {
