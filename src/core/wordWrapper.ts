8f52a7ef7d1a5f3c459240db63ec98c51c05de05
Jose Garrera <josegarrera@gmail.com>
2024-08-29 10:03:48 -0300

    green: general long word implementation

--- src/core/wordWrapper.ts
+++ src/core/wordWrapper.ts
@@ -2,5 +2,17 @@ export function wordWrapper(word: string, width: number): string {
   if (word.length <= width) {
     return word;
   }
-  return 'long\nword';
+  let wordGrouping: string[] = [];
+  const characters = word.split('');
+  let partialWord = '';
+  characters.forEach(char => {
+    if (partialWord.length < width) {
+      partialWord = partialWord + char;
+    } else {
+      wordGrouping.push(partialWord);
+      partialWord = char;
+    }
+  });
+  wordGrouping.push(partialWord);
+  return wordGrouping.join('\n');
 }
