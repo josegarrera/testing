7cee7588369290edd466dfd798fe531c14cd6470
Jose Garrera <josegarrera@gmail.com>
2024-08-29 17:19:14 -0300

    refactor: text as value object

--- src/core/wordWrapper.ts
+++ src/core/wordWrapper.ts
@@ -1,8 +1,9 @@
 export class ColumnWidth {
   private constructor(private readonly width: number) {}
   static create(width: number) {
-    if (width <= 0)
+    if (width <= 0) {
       throw new Error('width must be a positive number');
+    }
     return new ColumnWidth(width);
   }
   get value() {
@@ -10,17 +11,29 @@ export class ColumnWidth {
   }
 }
 
+export class WrappableText {
+  private constructor(private readonly text: string) {}
+  static create(text: string | null | undefined) {
+    if (text == null || text == undefined) {
+      text = '';
+    }
+    return new WrappableText(text);
+  }
+  get value() {
+    return this.text;
+  }
+}
+
 export function wordWrapper(
-  word: string | null | undefined,
+  word: WrappableText,
   width: ColumnWidth
 ): string {
-  if (word === null || word === undefined) return '';
-  if (word.length <= width.value) {
-    return word;
+  if (word.value.length <= width.value) {
+    return word.value;
   }
-  const wordTrimmed = word.trim();
+  const wordTrimmed = WrappableText.create(word.value.trim());
   let wordGrouping: string[] = [];
-  const characters = wordTrimmed.split('');
+  const characters = wordTrimmed.value.split('');
   let partialWord = '';
   characters.forEach(char => {
     if (partialWord.length < width.value) {
@@ -32,7 +45,7 @@ export function wordWrapper(
   });
   wordGrouping.push(partialWord);
   wordGrouping = wordGrouping.map(word => word.trim());
-  const hasTrailingWhitespace = word !== wordTrimmed;
+  const hasTrailingWhitespace = word.value !== wordTrimmed.value;
   return hasTrailingWhitespace
     ? '\n' + wordGrouping.join('\n')
     : wordGrouping.join('\n');
--- src/tests/wordWrapper.test.ts
+++ src/tests/wordWrapper.test.ts
@@ -1,35 +1,71 @@
-import { ColumnWidth, wordWrapper } from '../core/wordWrapper';
+import {
+  ColumnWidth,
+  wordWrapper,
+  WrappableText,
+} from '../core/wordWrapper';
 describe('The Word Wrap', () => {
   it('empty text does not need to be wrapped', () => {
-    expect(wordWrapper('', ColumnWidth.create(5))).toBe('');
-    expect(wordWrapper(null, ColumnWidth.create(5))).toBe('');
-    expect(wordWrapper(undefined, ColumnWidth.create(5))).toBe('');
+    expect(
+      wordWrapper(WrappableText.create(''), ColumnWidth.create(5))
+    ).toBe('');
+    expect(
+      wordWrapper(WrappableText.create(null), ColumnWidth.create(5))
+    ).toBe('');
+    expect(
+      wordWrapper(
+        WrappableText.create(undefined),
+        ColumnWidth.create(5)
+      )
+    ).toBe('');
   });
   it('small text does not need to be wrapped', () => {
-    expect(wordWrapper('hello', ColumnWidth.create(5))).toBe('hello');
+    expect(
+      wordWrapper(
+        WrappableText.create('hello'),
+        ColumnWidth.create(5)
+      )
+    ).toBe('hello');
   });
   it('words are wrapped when do not fit the column width', () => {
-    expect(wordWrapper('longword', ColumnWidth.create(4))).toBe(
-      'long\nword'
-    );
-    expect(wordWrapper('reallylongword', ColumnWidth.create(4))).toBe(
-      'real\nlylo\nngwo\nrd'
-    );
+    expect(
+      wordWrapper(
+        WrappableText.create('longword'),
+        ColumnWidth.create(4)
+      )
+    ).toBe('long\nword');
+    expect(
+      wordWrapper(
+        WrappableText.create('reallylongword'),
+        ColumnWidth.create(4)
+      )
+    ).toBe('real\nlylo\nngwo\nrd');
   });
   it('spaces are preferred for wrapping', () => {
-    expect(wordWrapper('abc def', ColumnWidth.create(4))).toBe(
-      'abc\ndef'
-    );
-    expect(wordWrapper('abc def ghi', ColumnWidth.create(4))).toBe(
-      'abc\ndef\nghi'
-    );
-    expect(wordWrapper(' abcd', ColumnWidth.create(4))).toBe(
-      '\nabcd'
-    );
+    expect(
+      wordWrapper(
+        WrappableText.create('abc def'),
+        ColumnWidth.create(4)
+      )
+    ).toBe('abc\ndef');
+    expect(
+      wordWrapper(
+        WrappableText.create('abc def ghi'),
+        ColumnWidth.create(4)
+      )
+    ).toBe('abc\ndef\nghi');
+    expect(
+      wordWrapper(
+        WrappableText.create(' abcd'),
+        ColumnWidth.create(4)
+      )
+    ).toBe('\nabcd');
   });
   it('does not allow for negative column width', () => {
     expect(() =>
-      wordWrapper('hello', ColumnWidth.create(-5))
+      wordWrapper(
+        WrappableText.create('hello'),
+        ColumnWidth.create(-5)
+      )
     ).toThrow(new Error('width must be a positive number'));
   });
 });
